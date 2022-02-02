b78100982c798093704308e2d848e5b43004d80d
Pedrotti, Giuliano <giuliano.pedrotti@gmail.com>
2022-02-02 20:23:09 -0300

    Ajustes para jogar novamente

--- src/jokenpo/DesafioJokenpo.java
+++ src/jokenpo/DesafioJokenpo.java
@@ -12,60 +12,88 @@ public class DesafioJokenpo {
 		int optionComputer;
 		String optionPlayerName = null;
 		String optionComputerName = null;
+		String novaPartida = "S";
 		
-		//Ler o teclado do usuário
-		Scanner scOption = new Scanner(System.in);
-		
-		//Executando o menu
-		menu();
-		
-		//Lendo opção do Jogador
-		optionPlayer = scOption.nextInt();
-		
-		//Analisando opção do Jogador
-		if (optionPlayer < 1 || optionPlayer > 3) {
-			System.out.println("Opção inválida por favor selecione as opções listadas acima!!");
-			main(null);
-		} else {
-			System.out.println("");
-			switch(optionPlayer) {
+		while (novaPartida.equalsIgnoreCase("S")) {
+			//Ler o teclado do usuário
+			Scanner scOption = new Scanner(System.in);
+			
+			//Executando o menu
+			menu();
+			
+			//Lendo opção do Jogador
+			optionPlayer = scOption.nextInt();
+			
+			//Analisando opção do Jogador
+			if (optionPlayer < 1 || optionPlayer > 3) {
+				System.out.println("Opção inválida por favor selecione as opções listadas acima!!");
+				main(null);
+			} else {
+				System.out.println("");
+				switch(optionPlayer) {
+				case 1:
+					optionPlayerName = "Pedra";
+					break;
+				case 2:
+					optionPlayerName = "Papel";
+					break;
+				case 3:
+					optionPlayerName = "Tesoura";
+					break;
+				}
+			}
+			
+			System.out.println("Você escolheu " + optionPlayerName);
+			
+			//Gerando a opção do computador
+			Random rnComputer = new Random();
+			optionComputer = rnComputer.nextInt(3) + 1;
+			switch(optionComputer) {
 			case 1:
-				optionPlayerName = "Pedra";
+				optionComputerName = "Pedra";
 				break;
 			case 2:
-				optionPlayerName = "Papel";
+				optionComputerName = "Papel";
 				break;
 			case 3:
-				optionPlayerName = "Tesoura";
+				optionComputerName = "Tesoura";
 				break;
 			}
+			System.out.println("Computador escolheu  " + optionComputerName);
+			
+			//Computando ganhador
+			if (optionPlayer == 1 && optionComputer ==1) {
+				System.out.println("O Jogo Empatou!!");
+			} else if ( optionPlayer == 1 && optionComputer == 2 ) {
+				System.out.println("O Computador Ganhou!!");
+			} else if ( optionPlayer == 1 && optionComputer == 3 ) {
+				System.out.println("Você Ganhou!!");
+			} else if ( optionPlayer == 2 && optionComputer == 1 ) {
+				System.out.println("Você Ganhou!!");
+			} else if ( optionPlayer == 2 && optionComputer == 2 ) {
+				System.out.println("O Jogo Empatou!!");
+			} else if ( optionPlayer == 2 && optionComputer == 3 ) {
+				System.out.println("O Computador Ganhou!!");
+			} else if ( optionPlayer == 3 && optionComputer == 1 ) {
+				System.out.println("O Computador Ganhou!!");
+			} else if ( optionPlayer == 3 && optionComputer == 2 ) {
+				System.out.println("Você Ganhou!!");
+			} else {
+				System.out.println("O Jogo Empatou!!");
+			}
+			
+			//Nova partida
+			System.out.print("");
+			System.out.println("Jogar novamente (S/N)?");
+			novaPartida = scOption.next();
 		}
 		
-		System.out.println("Você escolheu " + optionPlayerName);
+		System.out.println("F I M");
 		
-		//Gerando a opção do computador
-		Random rnComputer = new Random();
-		optionComputer = rnComputer.nextInt(3) + 1;
-		switch(optionComputer) {
-		case 1:
-			optionComputerName = "Pedra";
-			break;
-		case 2:
-			optionComputerName = "Papel";
-			break;
-		case 3:
-			optionComputerName = "Tesoura";
-			break;
-		}
-		System.out.println("Computador escolheu  " + optionComputerName);
-		
-		//Computando ganhador
-		if (optionPlayerName == optionComputerName) {
-			System.out.println("O Jogo Empatou!!");
-		} 
+
 	}
 	
-	public static void menu() {		
+	public static void menu() {
 		//Menu
 		System.out.println("----------------------------");
 		System.out.println("      J O K E N P O");
@@ -77,5 +105,4 @@ public class DesafioJokenpo {
 		System.out.println("");
 		System.out.println("Qual a sua opção:");
 	}
-
 }
